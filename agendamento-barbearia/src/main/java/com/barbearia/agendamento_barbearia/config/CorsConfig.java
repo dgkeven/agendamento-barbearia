d7b8ccb4cc304fc6b47dcaeb019fca1b1aeac478
Keven Mendes <dgkeven@outlook.com>
2025-11-02 12:24:53 -0300

    ajustes de inicialização

--- agendamento-barbearia/src/main/java/com/barbearia/agendamento_barbearia/config/CorsConfig.java
+++ agendamento-barbearia/src/main/java/com/barbearia/agendamento_barbearia/config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.barbearia.agendamento_barbearia.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
--- agendamento-barbearia/src/main/java/com/barbearia/agendamento_barbearia/modelos/Agendamento.java
+++ agendamento-barbearia/src/main/java/com/barbearia/agendamento_barbearia/modelos/Agendamento.java
@@ -1,11 +1,9 @@
 package com.barbearia.agendamento_barbearia.modelos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "agendamento")
 public class Agendamento {
 
     @Id
